0f640fd4b350e33e2c07b905e36f3fc229be85a7
Priyam <onepriyam@gmail.com>
2023-06-14 22:41:24 +0530

    initial commit

---
+++ ethereum/deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+
+const provider = new HDWalletProvider(
+  "symptom drink slim boring mix elevator supply palm walk boring equip surge",
+  // remember to change this to your own phrase!
+  "https://sepolia.infura.io/v3/6b6e4d30a15d4298a8f2de87fd76db28"
+  // remember to change this to your own endpoint!
+);
+console.log(Web3);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(compiledFactory.abi)
+    .deploy({ data: compiledFactory.evm.bytecode.object })
+    .send({ gas: "1400000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
